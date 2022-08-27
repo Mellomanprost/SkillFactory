c9c8c0c19339d0cf74fe5b552c01ee98d4db5895
Mellomanprost <anoppa@mail.ru>
2022-08-27 13:32:57 +0300

    Added exercise 4.1.5 and 4.1.17

---
+++ SkillFactory.Module4/Exercise415.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SkillFactory.Module4
+{
+    class Exercise415
+    {
+        public void TimeOfYear()
+        {
+            int currentDayInYear = 293;
+            bool isLeapYear = false;
+
+            bool isWinter =
+                !isLeapYear & (currentDayInYear >= 335 | currentDayInYear <= 59)
+                |
+                isLeapYear & (currentDayInYear >= 336 | currentDayInYear <= 60);
+
+            Console.WriteLine("Текущее время года - зима: {0}", isWinter);
+            Console.ReadKey();
+
+            var inv = true;
+            var result = !inv;
+            Console.WriteLine(result);
+        }
+    }
+}

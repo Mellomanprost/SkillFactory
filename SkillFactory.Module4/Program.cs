823903ab5441c7d7e4ac350a9eb8c045d9c0494e
Mellomanprost <anoppa@mail.ru>
2022-08-27 13:04:27 +0300

    Добавьте файлы проекта.

---
+++ SkillFactory.Module4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SkillFactory.Module4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int currentDayInYear = 293;
+            bool isLeapYear = false;
+
+            bool isWinter =
+                !isLeapYear & (currentDayInYear >= 335 | currentDayInYear <= 59)
+                |
+                isLeapYear & (currentDayInYear >= 336 | currentDayInYear <= 60);
+
+            Console.WriteLine("Текущее время года - зима: {0}", isWinter);
+            Console.ReadKey();
+
+            var inv = true;
+            var result = !inv;
+            Console.WriteLine(result);
+        }
+    }
+}
